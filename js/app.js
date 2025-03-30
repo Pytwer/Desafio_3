adc7400f0c0a4c4d5caf10feefc32301b6eaf272
Pytwer.Dev <122356458+Pytwer@users.noreply.github.com>
2025-03-30 10:21:53 -0300

    teste 2.0

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('nav ul').classList.toggle('active');
+    this.classList.toggle('active');
 });
+
+// Carrega Font Awesome dinamicamente
+const faScript = document.createElement('script');
+faScript.src = 'https://kit.fontawesome.com/a076d05399.js';
+faScript.crossOrigin = 'anonymous';
+document.head.appendChild(faScript);
